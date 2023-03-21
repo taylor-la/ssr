79c8630255fc81c797cd0420fb427a1ea39d568c
taylor-la <92021964+taylor-la@users.noreply.github.com>
2023-03-21 16:55:58 +0000

    update name selection

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,22 @@
 // ES6 or Vanilla JavaScript
+
+if (window.location.href.indexOf('dashboard') > 0) {
+    $('a.nhsuk-account-navigation__link').removeClass("nhsuk-account-navigation__link__current");
+    $('a.nhsuk-account-navigation__link[href$="/v8/dashboard"]').addClass("nhsuk-account-navigation__link__current");
+  }
+
+  if (window.location.href.indexOf('all-applications') > 0) {
+    $('a.nhsuk-account-navigation__link').removeClass("nhsuk-account-navigation__link__current");
+    $('a.nhsuk-account-navigation__link[href$="/v8/all-applications"]').addClass("nhsuk-account-navigation__link__current");
+  }
+
+  if (window.location.href.indexOf('settings') > 0) {
+    $('a.nhsuk-account-navigation__link').removeClass("nhsuk-account-navigation__link__current");
+    $('a.nhsuk-account-navigation__link[href$="/v8/settings"]').addClass("nhsuk-account-navigation__link__current");
+  }
+
+  if (window.location.href.indexOf('help') > 0) {
+    $('a.nhsuk-account-navigation__link').removeClass("nhsuk-account-navigation__link__current");
+    $('a.nhsuk-account-navigation__link[href$="/v8/help"]').addClass("nhsuk-account-navigation__link__current");
+  }
+  
