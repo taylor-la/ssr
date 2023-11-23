1c6eb455b8850c87b29c49d0685305e67e696f17
taylor-la <92021964+taylor-la@users.noreply.github.com>
2023-11-23 11:41:02 +0000

    applicant v19

--- app/routes.js
+++ app/routes.js
@@ -2016,6 +2016,181 @@ router.post('/applicant/v18/step-2/check-your-details-dpoa', function (req, res)
 
 })
 
+// applicant v19
+
+
+router.post('/applicant/v19/step-1/address-postcode', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var dpoa = req.session.data['dpoa']
+
+  // Check whether the variable matches a condition
+  if (dpoa == "no"){
+    // Send user to next page
+    res.redirect('/applicant/v19/step-1/poa')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/applicant/v19/step-1/address-postcode')
+  }
+
+})
+
+
+router.post('/applicant/v19/step-1/1-photo-id', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var passport = req.session.data['passport']
+
+  // Check whether the variable matches a condition
+  if (passport == "other"){
+    // Send user to next page
+    res.redirect('/applicant/v19/step-1/1-photo-id')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/applicant/v19/step-2/upload-photo')
+  }
+
+})
+
+router.post('/applicant/v19/step-2/upload-photo', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var photo1 = req.session.data['1-photo-id']
+
+  // Check whether the variable matches a condition
+  if (photo1 == "none"){
+    // Send user to next page
+    res.redirect('/applicant/v19/offline/no-photo-id')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/applicant/v19/step-2/upload-photo')
+  }
+
+})
+
+router.post('/applicant/v19/step-1/2-photo-id', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var poa = req.session.data['poa']
+
+  // Check whether the variable matches a condition
+  if (poa == "none"){
+    // Send user to next page
+    res.redirect('/applicant/v19/offline/no-poa')
+  } 
+
+  if (poa == "driving licence"){
+    // Send user to next page
+    res.redirect('/applicant/v19/step-1/no-dl')
+  } 
+
+  else {
+    // Send user to ineligible page
+    res.redirect('/applicant/v19/step-1/2-photo-id')
+  }
+
+})
+
+router.post('/applicant/v19/offline/no-photo-id', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var photo2 = req.session.data['2-photo-id']
+
+  // Check whether the variable matches a condition
+  if (photo2 == "none"){
+    // Send user to next page
+    res.redirect('/applicant/v19/offline/no-photo-id')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/applicant/v19/step-2/upload-photo')
+  }
+
+})
+
+
+router.post('/applicant/v19/step-2/delivery-postcode', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var delivery = req.session.data['delivery']
+
+  // Check whether the variable matches a condition
+  if (delivery == "different"){
+    // Send user to next page
+    res.redirect('/applicant/v19/step-2/delivery-postcode')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/applicant/v19/step-2/check-your-details-dpoa')
+  }
+
+})
+
+
+router.post('/applicant/v19/step-2/preferred-name', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var reviewPhoto = req.session.data['profile']
+
+  // Check whether the variable matches a condition
+  if (reviewPhoto == "yes"){
+    // Send user to next page
+    res.redirect('/applicant/v19/step-2/preferred-name')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/applicant/v19/step-2/upload-photo')
+  }
+
+})
+
+router.post('/applicant/v19/step-2/delivery', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var dpoa = req.session.data['dpoa']
+
+  // Check whether the variable matches a condition
+  if (dpoa == "no"){
+    // Send user to next page
+    res.redirect('/applicant/v19/step-2/delivery-postcode')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/applicant/v19/step-2/delivery')
+  }
+
+})
+
+router.post('/applicant/v19/step-2/check-your-details-poa', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var dpoa = req.session.data['dpoa']
+
+  // Check whether the variable matches a condition
+  if (dpoa == "no"){
+    // Send user to next page
+    res.redirect('/applicant/v19/step-2/check-your-details-poa')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/applicant/v19/step-2/check-your-details-dpoa')
+  }
+
+})
+
+router.post('/applicant/v19/step-2/check-your-details-dpoa', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var dpoa = req.session.data['dpoa']
+
+  // Check whether the variable matches a condition
+  if (dpoa == "no"){
+    // Send user to next page
+    res.redirect('/applicant/v19/step-2/check-your-details-poa')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/applicant/v19/step-2/check-your-details-dpoa')
+  }
+
+})
+
+
+
 // id checker v5
 
 router.post('/idchecker/v5/reject-dup', function (req, res) {
